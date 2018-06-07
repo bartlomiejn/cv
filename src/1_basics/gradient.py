de733e12c962020b5e5f6b6548a3e900e38da414
Bartłomiej Nowak <bartlomiej.nwk@gmail.com>
2018-06-07 21:37:49 +0200

    Added sobel/scharr gradient calculation

---
+++ src/1_basics/gradient.py
@@ -0,0 +1,37 @@
+import cv2
+from imageutils import image_arg
+from imageutils import wait_and_destroy_all_windows
+from enum import IntEnum
+
+
+class Operator(IntEnum):
+    SOBEL = -1
+    SCHARR = 0
+
+
+def show(operator, image, image_gray):
+    g_x = None
+    g_y = None
+    desc = None
+    if operator == Operator.SOBEL:
+        g_x = cv2.Sobel(image_gray, ddepth=cv2.CV_64F, dx=1, dy=0)
+        g_y = cv2.Sobel(image_gray, ddepth=cv2.CV_64F, dx=0, dy=1)
+        desc = "Sobel"
+    else:
+        g_x = cv2.Scharr(image_gray, ddepth=cv2.CV_64F, dx=1, dy=0)
+        g_y = cv2.Scharr(image_gray, ddepth=cv2.CV_64F, dx=0, dy=1)
+        desc = "Scharr"
+    g_x = cv2.convertScaleAbs(g_x)
+    g_y = cv2.convertScaleAbs(g_y)
+    image_sobel = cv2.addWeighted(g_x, 0.5, g_y, 0.5, 0)
+    cv2.imshow("Original", image)
+    cv2.imshow(f"{desc} X", g_x)
+    cv2.imshow(f"{desc} Y", g_y)
+    cv2.imshow(f"{desc} Combined", image_sobel)
+    wait_and_destroy_all_windows()
+
+
+image = image_arg()
+image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+show(Operator.SOBEL, image, image_gray)
+show(Operator.SCHARR, image, image_gray)
