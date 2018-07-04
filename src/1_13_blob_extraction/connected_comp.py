21f222d0c7b91a62571e86d80a172b7c2e2738d7
Bartłomiej Nowak <bartlomiej.nwk@gmail.com>
2018-07-04 20:44:45 +0200

    Added connected-component labeling

--- src/1_13_blob_extraction/connected_comp.py
+++ src/1_13_blob_extraction/connected_comp.py
@@ -10,7 +10,19 @@ t = threshold_local(v, 29, offset=15, method="gaussian")
 thresh = (v < t).astype("uint8") * 255
 cv2.imshow("License plate", image)
 cv2.imshow("Thresholded plate", thresh)
-labels = measure.label(thresh, neighbors=8, background=8)
+labels = measure.label(thresh, neighbors=8, background=0)
 mask = np.zeros(thresh.shape, dtype="uint8")
-print(f"Found {len(np.unique(labels))} blobs")
+print(f"Label count: {len(labels)}. Found {len(np.unique(labels))} unique "
+      f"blobs")
+for i, label in enumerate(np.unique(labels)):
+    if label == 0:
+        print("Label: 0 (background)")
+        continue
+    print(f"Label: {i} (foreground)")
+    labelMask = np.zeros(thresh.shape, dtype="uint8")
+    labelMask[labels == label] = 255
+    numPixels = cv2.countNonZero(labelMask)
+    if numPixels > 300 and numPixels < 1500:
+        mask = cv2.add(mask, labelMask)
+cv2.imshow(f"Large Blobs", mask)
 cv2.waitKey(0)
