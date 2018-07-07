ae7cdafd44cfdf627a14afef4a5371c8c10d0a34
Bartłomiej Nowak <bartlomiej.nwk@gmail.com>
2018-07-07 15:31:19 +0200

    Added LabHistogramGenerator

---
+++ src/10_image_descriptors/k_means_clustering/pyimagesearch/descriptors/labhistogramgenerator.py
@@ -0,0 +1,19 @@
+import cv2
+import imutils
+
+
+class LabHistogramGenerator:
+    def __init__(self, bins):
+        self.bins = bins
+
+    def describe(self, image, mask=None):
+        lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+        hist = cv2.calcHist(
+            [lab], [0, 1, 2], mask, self.bins, [0, 256, 0, 256, 0, 256]
+        )
+        if imutils.is_cv2():
+            hist = cv2.normalize(hist).flatten()
+        else:
+            # is_cv3()
+            hist = cv2.normalize(hist, hist).flatten()
+        return hist
