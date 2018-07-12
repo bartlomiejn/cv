9ff079820be3d624c84e8a39a4e764488ee39aa7
Bartłomiej Nowak <bartlomiej.nwk@gmail.com>
2018-07-12 20:53:53 +0100

    Added hu moments calculation for separate shapes

---
+++ src/10_image_descriptors/hu_moments.py
@@ -0,0 +1,14 @@
+import cv2
+from contourutils import get_contours
+
+image = cv2.imread("../../assets/planes.png")
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+cnts = get_contours(image, cv2.RETR_EXTERNAL)
+for idx, cnt in enumerate(cnts):
+    x, y, w, h = cv2.boundingRect(cnt)
+    roi = image[y:y+h, x:x+w]
+    hu_moments = cv2.HuMoments(cv2.moments(roi)).flatten()
+    print(f"Moments: {hu_moments}")
+    cv2.imshow(f"ROI #{idx+1}", roi)
+cv2.waitKey(0)
