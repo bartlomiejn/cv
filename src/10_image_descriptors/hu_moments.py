97d27161d43fbfce287f5cff4bf9568d8e4ed80c
Bartłomiej Nowak <bartlomiej.nwk@gmail.com>
2018-07-13 19:25:45 +0100

    Added shapes image and small changes to hu moments file

--- src/10_image_descriptors/hu_moments.py
+++ src/10_image_descriptors/hu_moments.py
@@ -1,9 +1,8 @@
 import cv2
 from contourutils import get_contours
 
-image = cv2.imread("../../assets/planes.png")
+image = cv2.imread("../../assets/more_shapes_example.png")
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
 cnts = get_contours(image, cv2.RETR_EXTERNAL)
 for idx, cnt in enumerate(cnts):
     x, y, w, h = cv2.boundingRect(cnt)
@@ -11,4 +10,7 @@ for idx, cnt in enumerate(cnts):
     hu_moments = cv2.HuMoments(cv2.moments(roi)).flatten()
     print(f"Moments: {hu_moments}")
     cv2.imshow(f"ROI #{idx+1}", roi)
+
+hu_moments = cv2.HuMoments(cv2.moments(image)).flatten()
+print(f"[IMAGE] Moments: {hu_moments}")
 cv2.waitKey(0)
