537b2bd2836a69d56120b93fb4e5366c463f77db
Bartłomiej Nowak <bartlomiej.nwk@gmail.com>
2018-09-23 14:08:01 +0200

    Fixed package name

--- src/10_image_descriptors/lbp_image_search/search_shirts.py
+++ src/10_image_descriptors/lbp_image_search/search_shirts.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-from pyimagesearch import LocalBinaryPatterns
+from descriptors import LocalBinaryPatterns
 from imutils import paths
 import numpy as np
 import argparse
