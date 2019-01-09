1341ef0e1750b68602e21ebfca1017e97224f188
Bartłomiej Nowak <bartlomiej.nwk@gmail.com>
2019-01-09 13:18:18 +0100

    Gradient descent example, for some reason doesn't work correctly

--- src/machine_learning/gradient-descent/gradient_descent.py
+++ src/machine_learning/gradient-descent/gradient_descent.py
@@ -23,3 +23,60 @@ ap = ArgumentParser()
 ap.add_argument("-e", "--epochs", type=float, default=100, help="# of epochs")
 ap.add_argument("-a", "--alpha", type=float, default=0.01, help="Learning rate")
 args = vars(ap.parse_args())
+
+# Generate a 2 class classification problem with 1000 data points, where each
+# point is a 2-dim feature vector
+X, y = make_blobs(
+    n_samples=1000,
+    n_features=2,
+    centers=2,
+    cluster_std=1.5,
+    random_state=1
+)
+y = y.reshape((y.shape[0], 1))
+
+# Insert a column of 1s as the last entry in the feature matrix
+X = np.c_[X, np.ones(X.shape[0])]
+
+train_X, test_X, train_y, test_y = train_test_split(
+    X,
+    y,
+    test_size=0.5,
+    random_state=42
+)
+
+# Initialize the weight matrix with a uniform distribution and list of losses
+W = np.random.randn(X.shape[1], 1)
+losses = []
+
+for epoch in np.arange(0, args["epochs"]):
+    preds = sigmoid_activation(train_X.dot(W))
+    error = preds - train_y
+    loss = np.sum(error ** 2)
+    losses.append(loss)
+    # Gradient descent update is the dot product between features and prediction
+    # errors. We take a small step towards a set of more optimal parameters
+    # based on the alpha parameter
+    gradient = train_X.T.dot(error)
+    W += -args["alpha"] * gradient
+    if epoch == 0 or (epoch + 1) % 5 == 0:
+        print(f"[INFO] epoch={int(epoch + 1)}, loss={loss:.7f}")
+
+print("[INFO] Evaluating")
+preds = predict(test_X, W)
+print(classification_report(test_y, preds))
+
+# Plot the classification data
+plt.style.use("ggplot")
+plt.figure()
+plt.title("data")
+plt.scatter(test_X[:, 0], test_X[:, 1], marker="o", c=test_y, s=30)
+
+# Plot the loss over time
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, args["epochs"]), losses)
+plt.title("Training loss")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss")
+plt.show()
