84d5f7ac43d9062aec21159293bc0d37e2aaa6e2
Bartłomiej Nowak <bartlomiej.nwk@gmail.com>
2019-01-11 13:34:17 +0100

    Added perceptron and, or, xor examples

---
+++ src/machine_learning/neural-networks/perceptron/perc_or.py
@@ -0,0 +1,16 @@
+import sys; sys.path.insert(0, '../..')
+from support.nn.perceptron import Perceptron
+import numpy as np
+
+# OR
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([[0], [1], [1], [1]])
+
+print("[INFO] Training")
+p = Perceptron(X.shape[1], alpha=0.1)
+p.fit(X, y, epochs=20)
+
+print("[INFO] Evaluating")
+for x, target in zip(X, y):
+    pred = p.predict(x)
+    print(f"[INFO] data={x} ground-truth={target[0]} pred={pred}")
