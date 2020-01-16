0eec6c89b9e0dad420ed541f69205233b5392124
Bartłomiej Nowak <bartlomiej.nwk@gmail.com>
2020-01-16 20:18:32 +0100

    Replace tabs with spaces

--- src/machine_learning/support/preprocessing/preprocessor.py
+++ src/machine_learning/support/preprocessing/preprocessor.py
@@ -3,26 +3,26 @@ from keras.preprocessing.image import img_to_array
 
 
 class ImageToArrayPreprocessor:
-	def __init__(self, data_format=None):
-		self.data_format = data_format
+    def __init__(self, data_format=None):
+        self.data_format = data_format
 
-	def process(self, image):
-		"""
+    def process(self, image):
+        """
         Returns the image with channel ordering based on Keras configuration
         json.
         """
-		return img_to_array(image, data_format=self.data_format)
+        return img_to_array(image, data_format=self.data_format)
 
 
 class ResizePreprocessor:
-	def __init__(self, width, height, interpolation=cv2.INTER_AREA):
-		self.width = width
-		self.height = height
-		self.interpolation = interpolation
+    def __init__(self, width, height, interpolation=cv2.INTER_AREA):
+        self.width = width
+        self.height = height
+        self.interpolation = interpolation
 
-	def preprocess(self, image):
-		return cv2.resize(
-			image,
-			(self.width, self.height),
-			interpolation=self.interpolation
-		)
+    def preprocess(self, image):
+        return cv2.resize(
+            image,
+            (self.width, self.height),
+            interpolation=self.interpolation
+        )
